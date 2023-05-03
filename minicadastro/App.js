90e19e6bad0b98f6c99b44efa5c3127cfcbf6191
codingscode <heronnestg@hotmail.com>
2023-05-02 23:30:27 -0300

    parte 1

---
+++ minicadastro/App.js
@@ -0,0 +1,27 @@
+
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+
+const App = () => {
+
+   return (
+      <View style={styles.container} >
+         <Text>Teste</Text>
+         
+      </View>
+   )
+}
+
+
+
+const styles = StyleSheet.create({
+   container: {
+      backgroundColor: '#F1EA55',
+      width: 270,
+      height: 150,
+      
+   }
+})
+
+export default App
